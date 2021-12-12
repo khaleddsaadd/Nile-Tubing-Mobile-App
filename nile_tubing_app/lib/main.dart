1de6eec078e0d26c544ef20a117110e5be3dc38f
Khaled Saad <74107324+khaleddsaadd@users.noreply.github.com>
2021-12-12 23:44:36 +0200

    Update main.dart

--- nile_tubing_app/lib/main.dart
+++ nile_tubing_app/lib/main.dart
@@ -1,27 +1,8 @@
 import 'package:flutter/material.dart';
-//import 'package:nile_tubing_app/Home.dart';
-import 'package:nile_tubing_app/Rides.dart';
-<<<<<<< Updated upstream
-import 'package:nile_tubing_app/screens/SelectedRide.dart';
-import 'package:nile_tubing_app/screens/Payment.dart';
-
-void main() {
-  runApp(MaterialApp(
-    initialRoute: '/',
-    //routes: {'/': (context) => Rides()},
-    //routes: {'/': (context) => SelectedRide()},
-    //routes: {'/': (context) => Home()},
-    routes: {'/': (context) => Payment()},
-  ));
-=======
-import 'package:nile_tubing_app/SelectedRide.dart';
-import 'package:nile_tubing_app/checkOut.dart';
-import 'Payment.dart';
-import 'paymentForm.dart';
+import 'screens/Home.dart';
 
 void main() {
   runApp(MaterialApp(initialRoute: '/', routes: {
     '/': (context) => Home(),
   }));
->>>>>>> Stashed changes
 }
