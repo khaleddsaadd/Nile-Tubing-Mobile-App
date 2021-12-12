0dc6e51b3da9b7d85fe59874f01909dd525a01b1
Khaled Saad <74107324+khaleddsaadd@users.noreply.github.com>
2021-12-12 11:44:25 +0200

    Rides Updates

--- nile_tubing_app/lib/Rides.dart
+++ nile_tubing_app/lib/Rides.dart
@@ -1,25 +1,63 @@
 // ignore: avoid_web_libraries_in_flutter
 // ignore_for_file: use_key_in_widget_constructors, unused_import, unused_import, duplicate_ignore, prefer_const_constructors, file_names
 import 'package:flutter/material.dart';
+import 'home_widget.dart';
 
-class Rides extends StatelessWidget {
+class Rides extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  State<Rides> createState() => _RidesState();
+}
+
+class _RidesState extends State<Rides> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: ListView(
       children: [
+        // ignore: sized_box_for_whitespace
         Container(
-          
           // ignore: prefer_const_literals_to_create_immutables
           child:
               Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
             Image.asset('assets/NTLogo.png'),
-            Icon(Icons.shopping_cart, size: 35, color: Colors.white)
+            Padding(
+              padding: EdgeInsets.only(left: 30),
+              child: Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.shopping_cart,
+                        size: 35, color: Color.fromRGBO(0, 46, 92, 1)),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.person_outline_rounded,
+                          size: 35, color: Colors.yellow)),
+                ],
+              ),
+            )
           ]),
           height: 100,
         ),
-        
+        Column(
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            Padding(
+              padding:
+                  EdgeInsets.only(top: 40), //apply padding to some sides only
+              child: Text("Nile Tubing",
+                  style: TextStyle(
+                      fontSize: 50, color: Color.fromRGBO(0, 46, 92, 1)),
+                  textAlign: TextAlign.center),
+            ),
+            Text("Rides",
+                style: TextStyle(
+                    fontSize: 50, color: Color.fromRGBO(0, 46, 92, 1)),
+                textAlign: TextAlign.center),
+            Image.asset('assets/Rides.png'),
+          ],
+        ),
         Padding(
           padding: EdgeInsets.only(top: 25), //apply padding to some sides only
           child: Text("Single Tubing Ride",
@@ -60,6 +98,33 @@ class Rides extends StatelessWidget {
         Padding(
           padding: EdgeInsets.only(top: 25), //apply padding to some sides only
           child: Text("250 EGP",
+              style: TextStyle(fontSize: 20, color: Colors.black),
+              textAlign: TextAlign.center),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 15),
+          child: Row(
+            // ignore: prefer_const_literals_to_create_immutables
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(onPressed: () {}, child: Text('Book Now')),
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 25), //apply padding to some sides only
+          child: Text("Group Tubing Ride",
+              style: TextStyle(fontSize: 30, color: Colors.black),
+              textAlign: TextAlign.center),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: Image.asset('assets/group.jpg'),
+        ),
+        Padding(
+          padding: EdgeInsets.only(top: 25), //apply padding to some sides only
+          child: Text("350 EGP",
               style: TextStyle(fontSize: 20, color: Colors.black),
               textAlign: TextAlign.center),
         ),
