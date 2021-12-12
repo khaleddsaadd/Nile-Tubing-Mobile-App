0aa7adc87e65b07cba11222fcdf2b56dfc4dfe79
ATamer24 <mido.tamer2000@hotmail.com>
2021-12-12 21:50:46 +0200

    Cart

---
+++ nile_tubing_app/lib/checkOut.dart
@@ -0,0 +1,196 @@
+import 'package:flutter/material.dart';
+
+class CheckOut extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+  
+  return Scaffold(
+    body: ListView(children:[
+Container(
+          height: 100,
+          child: Row(children: [
+            Image.asset('assets/NTLogo.png'),
+            SizedBox(
+              width: 220,
+            ),
+            Icon(Icons.person_outline_rounded, size: 35, color: Colors.yellow),
+          ]),
+        ),
+Padding(
+          padding: EdgeInsets.only(top: 10,bottom:10), //apply padding to some sides only
+          child: Text("Proceed To Payment",
+              style: TextStyle(fontSize: 26 , color: Colors.black),
+              textAlign: TextAlign.center),
+        ),
+        
+Padding(padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 15),
+child: Container( 
+ decoration: BoxDecoration(
+  borderRadius: BorderRadius.circular(10),
+  color: Colors.white,
+  boxShadow: [
+  BoxShadow(
+  color: Colors.grey.withOpacity(0.5),
+  spreadRadius: 2,
+  blurRadius: 5,
+  ),
+  ]),
+ height: 150,
+ width: 160,
+
+child:Row(children: [
+      Align (alignment: Alignment.centerLeft,
+      child:Column(
+      children: [
+      Container(
+      width: 110,
+      height:150,
+      decoration: BoxDecoration(
+      image: DecorationImage(
+      image: AssetImage("assets/Background.jpg"),
+      fit: BoxFit.cover,
+                          ),
+      borderRadius: BorderRadius.circular(5),
+                           ),
+    ),
+   ],
+  )
+  )
+        ,SizedBox(width: 20),
+        Column(children: [ Padding(padding:const EdgeInsets.only(top: 5.0),
+        child:Align(alignment: Alignment.topCenter,
+        child:Container(
+          width: 220,
+          height:40,
+          child: Align (alignment: Alignment.center,
+          child: Text("TripName",
+          style: TextStyle(
+                           fontWeight: FontWeight.bold,fontSize: 22.0
+                          )
+         ),
+        )
+       )
+      )
+     ),
+       
+
+       Padding(padding:const EdgeInsets.only(top: 15.0,bottom:20.0 ),
+        child:Align(alignment: Alignment.topCenter,
+        child:Container(
+          width: 220,
+          height:30,
+           
+ child: Align (alignment: Alignment.center,
+          child: Text("TripDate",
+          style: TextStyle(
+                           fontSize: 18.0
+                          )
+         ),
+        )
+       )
+      )
+     ),
+     Align(alignment: Alignment.topLeft,
+     child: Container(child:Row(children: [
+//Add Icon          
+          Container(
+          width: 20,
+          height:20,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(4.0),
+          color: Colors.blue[300],
+          ),
+          child: Icon(Icons.remove,
+          size:15.0
+          )
+          ),
+//Counter 
+          Padding(padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Text("1", style: TextStyle(fontSize: 16.0,fontWeight: FontWeight.bold)
+          )
+          ) ,       
+//Remove Icon         
+          Container(
+          width: 20,
+          height:20,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(4.0),
+          color: Colors.grey[300],
+          ),
+          child: Icon(Icons.add,
+          size:15.0
+          )
+          ),
+          SizedBox(width: 85),
+          Text("Total: ")
+          ]) 
+        
+ )  )
+   ,
+
+        ],),
+       
+        ],),
+   )
+  ),
+  Padding(padding: const EdgeInsets.symmetric(horizontal: 15),
+  child: Container(
+          width: 50,
+          height: 80,
+          child: 
+          Align(alignment:Alignment.center,
+          child: Text("Sub Total: 300 USD", 
+          style: TextStyle(
+            fontSize:26,
+              fontWeight: FontWeight.bold
+            )
+          ),),
+          ),
+ ),
+   SizedBox(height: 80),
+   Padding(padding: const EdgeInsets.only(top:80),
+   child:Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  // ignore: prefer_const_literals_to_create_immutables
+children: [
+                    ButtonTheme(
+                    minWidth: 150.0,
+                    height: 50.0,
+                    child:
+                    RaisedButton(
+                    onPressed: () {},
+                    color: Color(0x4DFFFFFF).withOpacity(0.8),
+                    shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                    child: Text("Add More",
+                    style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.yellow,
+                    fontFamily: 'Cairo')
+                    ),
+                    ),
+                    ),
+  ButtonTheme(
+  minWidth: 150.0,
+  height: 50.0,
+  child:
+  RaisedButton(
+  onPressed: () {},
+  color: Color(0xff123456).withOpacity(0.8),
+  shape: RoundedRectangleBorder(
+  borderRadius: BorderRadius.circular(20)),
+  child: Text("Check Out",style: TextStyle(
+  fontSize: 15,
+  color: Colors.white,
+  fontFamily: 'Cairo')
+  )
+  ),
+  ),
+                  ])
+
+
+   )
+
+]),
+);  
+  }
+  }
+ 
