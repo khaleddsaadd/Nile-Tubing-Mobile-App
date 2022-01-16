96c0c7d25310641a351b71adc4459cc4d82d69b4
maielghandour <74146386+maielghandour@users.noreply.github.com>
2022-01-17 01:18:05 +0200

    Selected ride from firestore

--- nile_tubing_app/lib/screens/SelectedRide.dart
+++ nile_tubing_app/lib/screens/SelectedRide.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:nile_tubing_app/screens/checkOut.dart';
 import '../modules/home_widget.dart';
 import 'package:nile_tubing_app/screens/checkOut.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:nile_tubing_app/model/rides.dart';
 import 'signin.dart';
 import 'Rides.dart';
 import 'drawer.dart';
@@ -9,6 +11,7 @@ import 'drawer.dart';
 class SelectedRide extends StatelessWidget {
   String? ID;
   SelectedRide({this.ID});
+  CollectionReference users = FirebaseFirestore.instance.collection('Rides');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,159 +19,188 @@ class SelectedRide extends StatelessWidget {
         body: ListView(
           padding: EdgeInsets.all(15),
           children: [
-            Text("$ID"),
-            Container(
-              // ignore: prefer_const_literals_to_create_immutables
-              child: Row(children: [
-                IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: Icon(Icons.arrow_back)),
-                Image.asset('assets/NTLogo.png'),
-                Padding(
-                  padding: EdgeInsets.only(left: 83),
-                  child: Row(
+            FutureBuilder<DocumentSnapshot>(
+              future: users.doc(ID).get(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<DocumentSnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return Text("Something went wrong");
+                }
+
+                if (snapshot.hasData && !snapshot.data!.exists) {
+                  return Text("Document does not exist");
+                }
+
+                if (snapshot.connectionState == ConnectionState.done) {
+                  Map<String, dynamic> data =
+                      snapshot.data!.data() as Map<String, dynamic>;
+                  ridesmodel r = ridesmodel(
+                      rideName: data['Name'],
+                      ridePrice: int.parse(data['Price']));
+
+                  return Column(
                     children: [
-                      IconButton(
-                        onPressed: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => CheckOut())),
-                        icon: Icon(Icons.local_grocery_store,
-                            size: 35, color: Color(0xff123456)),
+                      // Text("${r.rideName} "),
+                      // Text("${data['Price']} ")
+                      Container(
+                        // ignore: prefer_const_literals_to_create_immutables
+                        child: Row(children: [
+                          IconButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              icon: Icon(Icons.arrow_back)),
+                          Image.asset('assets/NTLogo.png'),
+                          Padding(
+                            padding: EdgeInsets.only(left: 83),
+                            child: Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () => Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => CheckOut())),
+                                  icon: Icon(Icons.local_grocery_store,
+                                      size: 35, color: Color(0xff123456)),
+                                ),
+                                IconButton(
+                                    icon: new Icon(Icons.person_outline_rounded,
+                                        size: 35, color: Colors.yellow[700]),
+                                    onPressed: () => Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => SignIn()),
+                                        )),
+                              ],
+                            ),
+                          )
+                        ]),
+                        height: 100,
                       ),
-                      IconButton(
-                          icon: new Icon(Icons.person_outline_rounded,
-                              size: 35, color: Colors.yellow[700]),
-                          onPressed: () => Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => SignIn()),
-                              )),
-                    ],
-                  ),
-                )
-              ]),
-              height: 100,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 30),
-              child: Text("Single Tubing Rides",
-                  style: TextStyle(
-                      fontSize: 25,
-                      color: Color(0xff123456),
-                      fontFamily: 'Cairo'),
-                  textAlign: TextAlign.center),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            new Container(
-                padding: EdgeInsets.all(30),
-                height: 350,
-                decoration: new BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(12),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.grey,
-                      blurRadius: 30.0, // soften the shadow
-                      spreadRadius: 4.0, //extend the shadow
-                      offset: Offset(
-                        0, // Move to right 10  horizontally
-                        5, // Move to bottom 10 Vertically
+                      Padding(
+                        padding: EdgeInsets.only(top: 30),
+                        child: Text("${r.rideName} ",
+                            style: TextStyle(
+                                fontSize: 25,
+                                color: Color(0xff123456),
+                                fontFamily: 'Cairo'),
+                            textAlign: TextAlign.center),
                       ),
-                    )
-                  ],
-                ),
-                child: Column(children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Column(children: const [
-                      Text('200 EGP',
-                          style: TextStyle(
-                              fontSize: 30,
-                              color: Color(0xff123456),
-                              fontFamily: 'Cairo'),
-                          textAlign: TextAlign.left),
                       SizedBox(
-                        height: 10,
+                        height: 40,
                       ),
-                      Text('Per person',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Color(0xff123456),
-                            fontFamily: 'Cairo',
+                      Container(
+                          padding: EdgeInsets.all(30),
+                          height: 350,
+                          decoration: new BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors.grey,
+                                blurRadius: 30.0, // soften the shadow
+                                spreadRadius: 4.0, //extend the shadow
+                                offset: Offset(
+                                  0, // Move to right 10  horizontally
+                                  5, // Move to bottom 10 Vertically
+                                ),
+                              )
+                            ],
                           ),
-                          textAlign: TextAlign.left),
-                      SizedBox(
-                        height: 25,
+                          child: Column(children: [
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: Column(children: [
+                                Text("${r.ridePrice} ",
+                                    style: TextStyle(
+                                        fontSize: 25,
+                                        color: Color(0xff123456),
+                                        fontFamily: 'Cairo'),
+                                    textAlign: TextAlign.center),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text('Per person',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color: Color(0xff123456),
+                                      fontFamily: 'Cairo',
+                                    ),
+                                    textAlign: TextAlign.left),
+                                SizedBox(
+                                  height: 25,
+                                ),
+                                Text('Date',
+                                    style: TextStyle(
+                                        fontSize: 25,
+                                        color: Color(0xff123456),
+                                        fontFamily: 'Cairo'),
+                                    textAlign: TextAlign.left),
+                                SizedBox(
+                                  height: 25,
+                                ),
+                                Text('Capacity',
+                                    style: TextStyle(
+                                        fontSize: 25,
+                                        color: Color(0xff123456),
+                                        fontFamily: 'Cairo'),
+                                    textAlign: TextAlign.left),
+                              ]),
+                            ),
+                            SizedBox(
+                              height: 50,
+                            ),
+                            Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  ElevatedButton(
+                                      style: ButtonStyle(
+                                          backgroundColor:
+                                              MaterialStateProperty.all<Color>(
+                                                  Colors.white30)),
+                                      onPressed: () {},
+                                      child: Text('Add to cart',
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              color: Colors.yellow,
+                                              fontFamily: 'Cairo'))),
+                                  ElevatedButton(
+                                      style: ButtonStyle(
+                                          backgroundColor:
+                                              MaterialStateProperty.all<Color>(
+                                                  Color(0xff123456))),
+                                      onPressed: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => CheckOut()),
+                                        );
+                                      },
+                                      child: Text('Checkout',
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              color: Colors.white,
+                                              fontFamily: 'Cairo')))
+                                ])
+                          ])),
+                      Padding(
+                        padding: EdgeInsets.only(top: 40),
+                        child: Text("Description",
+                            style: TextStyle(
+                                fontSize: 25,
+                                color: Color(0xff123456),
+                                fontFamily: 'Cairo'),
+                            textAlign: TextAlign.center),
                       ),
-                      Text('Date',
-                          style: TextStyle(
-                              fontSize: 25,
-                              color: Color(0xff123456),
-                              fontFamily: 'Cairo'),
-                          textAlign: TextAlign.left),
                       SizedBox(
-                        height: 25,
+                        height: 150,
                       ),
-                      Text('Capacity',
-                          style: TextStyle(
-                              fontSize: 25,
-                              color: Color(0xff123456),
-                              fontFamily: 'Cairo'),
-                          textAlign: TextAlign.left),
-                    ]),
-                  ),
-                  SizedBox(
-                    height: 50,
-                  ),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        ElevatedButton(
-                            style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all<Color>(
-                                        Colors.white30)),
-                            onPressed: () {},
-                            child: Text('Add to cart',
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    color: Colors.yellow,
-                                    fontFamily: 'Cairo'))),
-                        ElevatedButton(
-                            style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all<Color>(
-                                        Color(0xff123456))),
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => CheckOut()),
-                              );
-                            },
-                            child: Text('Checkout',
-                                style: TextStyle(
-                                    fontSize: 15,
-                                    color: Colors.white,
-                                    fontFamily: 'Cairo')))
-                      ])
-                ])),
-            Padding(
-              padding: EdgeInsets.only(top: 40),
-              child: Text("Description",
-                  style: TextStyle(
-                      fontSize: 25,
-                      color: Color(0xff123456),
-                      fontFamily: 'Cairo'),
-                  textAlign: TextAlign.center),
-            ),
-            SizedBox(
-              height: 150,
+                    ],
+                  );
+                }
+                return Text("loading");
+              },
             ),
           ],
         ));
