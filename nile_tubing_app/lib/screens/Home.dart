b276368b664782a2f1f2fd58b4f6c1cee6496c06
ATamer24 <mido.tamer2000@hotmail.com>
2022-01-21 22:34:41 +0200

    updated

--- nile_tubing_app/lib/screens/Home.dart
+++ nile_tubing_app/lib/screens/Home.dart
@@ -180,79 +180,139 @@ class Home extends StatelessWidget {
                   Events event = Events(
                       EventName: document['Name'],
                       EventPrice: document['Price'],
-                      //EventDescription: document['Description'],
+                      // EventDescription: document['Description'],
                       EventType: document['Type'],
-                       EventStart: document['Start Date'].toDate().toString(),
+                      //  EventStart: document['Start Date'].toDate().toString(),
                       // EventEnd:document['End Date'].toDate().toString()
                     
                       );
                       
                   return Column(
                     children: [
+                      Row(children: [
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: Column(
+                        children: [
+                          Container(
+                            width: 110,
+                            height: 150,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage("assets/Background.jpg"),
+                                fit: BoxFit.cover,
+                              ),
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                          )
+                        ],
+                      )),
+                  Container(
+                      width: MediaQuery.of(context).size.width - 140,
+                      child: Column(children: [
+                        Padding(
+                            padding: const EdgeInsets.only(top: 5.0),
+                            child: Align(
+                                alignment: Alignment.topCenter,
+                                child: Container(
+                                    height: 40,
+                                    child: Align(
+                                      alignment: Alignment.center,
+                                      child: Text("${event.EventName}",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 22.0)),
+                                    )))),
+                        Padding(
+                            padding:
+                                const EdgeInsets.only(top: 15.0, bottom: 20.0),
+                            child: Align(
+                                alignment: Alignment.topCenter,
+                                child: Container(
+                                    height: 30,
+                                    child: Align(
+                                      alignment: Alignment.center,
+                                      child: Text("${event.EventPrice}",
+                                          style: TextStyle(fontSize: 18.0)),
+                                    )))),
+Padding(
+                            padding:
+                                const EdgeInsets.only(top: 15.0, bottom: 20.0),
+                            child: Align(
+                                alignment: Alignment.topCenter,
+                                child: Container(
+                                    height: 30,
+                                    child: Align(
+                                      alignment: Alignment.center,
+                                      child: Text("${event.EventType}",
+                                          style: TextStyle(fontSize: 18.0)),
+                                    )))),                        
+                      ]))
+                ])
                       // Text("${event.EventName}"),
                       // Text("${event.EventPrice}")
-                      DataTable(  
-              columns: [  
+              //         DataTable(  
+              // columns: [  
                
-                DataColumn(label: Text(  
-                    'Name',  
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)  
-                )),  
-                DataColumn(label: Text(  
-                    'Price',  
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)  
-                )),
+              //   DataColumn(label: Text(  
+              //       'Name',  
+              //       style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)  
+              //   )),  
+              //   DataColumn(label: Text(  
+              //       'Price',  
+              //       style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)  
+              //   )),
                
-                DataColumn(label: Text(  
-                    'Type',  
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)  
-                )),
-                  DataColumn(label: Text(  
-                    'Description',  
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)  
-                )),
-                DataColumn(label: Text(  
-                    'Start Date',  
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)  
-                )),
-                //DataColumn(label: Text(  
-                //     'End Date',  
-                //     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)  
-                // )),
-                DataColumn(label: Text(  
-                    'Test',  
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)  
-                )),
+              //   DataColumn(label: Text(  
+              //       'Type',  
+              //       style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)  
+              //   )),
+              //   //   DataColumn(label: Text(  
+              //   //     'Description',  
+              //   //     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)  
+              //   // )),
+              //   // DataColumn(label: Text(  
+              //   //     'Start Date',  
+              //   //     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)  
+              //   // )),
+              //   //DataColumn(label: Text(  
+              //   //     'End Date',  
+              //   //     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)  
+              //   // )),
+              //   DataColumn(label: Text(  
+              //       'Test',  
+              //       style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)  
+              //   )),
                 
-                ],  
-              rows: [  
-                DataRow(cells: [  
+              //   ],  
+              // rows: [  
+              //   DataRow(cells: [  
 
-                  DataCell(Text("${event.EventName}")),  
-                  DataCell(Text("${event.EventPrice}")),
-                  DataCell(Text("${event.EventType}")),
-                  DataCell(Text("${event.EventDescription}")),
-                  DataCell(Text("${event.EventStart}")),
+              //     DataCell(Text("${event.EventName}")),  
+              //     DataCell(Text("${event.EventPrice}")),
+              //     DataCell(Text("${event.EventType}")),
+              //     // DataCell(Text("${event.EventDescription}")),
+              //     // DataCell(Text("${event.EventStart}")),
              
-                  DataCell(RaisedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => CheckOut()),
-                      );
-                    },
-                    color: Color(0xff123456).withOpacity(1),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Text("Book Now",
-                        style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.white,
-                            fontFamily: 'Cairo')),
-                  ),)
-                ]),  
+              //     DataCell(RaisedButton(
+              //       onPressed: () {
+              //         Navigator.push(
+              //           context,
+              //           MaterialPageRoute(builder: (context) => CheckOut()),
+              //         );
+              //       },
+              //       color: Color(0xff123456).withOpacity(1),
+              //       shape: RoundedRectangleBorder(
+              //           borderRadius: BorderRadius.circular(20)),
+              //       child: Text("Book Now",
+              //           style: TextStyle(
+              //               fontSize: 15,
+              //               color: Colors.white,
+              //               fontFamily: 'Cairo')),
+              //     ),)
+              //   ]),  
 
-                    ],)
+              //       ],)
                     ]);
                 }).toList(),
               );
