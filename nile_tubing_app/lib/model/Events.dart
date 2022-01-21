d7a9cbe40bdb956cd7d50f3c93b5127430bd6dac
ATamer24 <mido.tamer2000@hotmail.com>
2022-01-21 13:45:14 +0200

    Events Model

--- nile_tubing_app/lib/model/Events.dart
+++ nile_tubing_app/lib/model/Events.dart
@@ -1,18 +1,18 @@
-class Events{
-  final String EventName;
-  final String EventID;
-  final String EventDescription;
-  final String EventType;
-  final DateTime EventStart;
-  final DateTime EventEnd;
-  
+class Events {
+  final String? EventName;
+  final String? EventID;
+  final String? EventDescription;
+  final String? EventType;
+  final DateTime? EventStart;
+  final DateTime? EventEnd;
+  final String? EventPrice;
 
-Events(
-  this.EventName,
-  this.EventID,
-  this.EventDescription,
-  this.EventType,
-  this.EventStart,
-  this.EventEnd
-  );
+  Events(
+      {this.EventName,
+      this.EventID,
+      this.EventDescription,
+      this.EventType,
+      this.EventStart,
+      this.EventEnd,
+      this.EventPrice});
 }
